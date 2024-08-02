03931e02fdadd6420adff80cebe46bc7cb20ccca
Tanay <tdmorakhia26@gmail.com>
2024-08-02 22:35:18 +0530

    Updates

---
+++ precache-manifest.9ecb6a036fa33b3adc922dbbef12a161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657aca04a5ecbb8783b7347029117b59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947b6760e074c9ffac80",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e41da8d48ff2c4cbadad",
+    "url": "/static/js/2.31a66e6b.chunk.js"
+  },
+  {
+    "revision": "0a0b602d25062375dacd",
+    "url": "/static/js/3.40a56a06.chunk.js"
+  },
+  {
+    "revision": "947b6760e074c9ffac80",
+    "url": "/static/js/main.ccb26ff2.chunk.js"
+  },
+  {
+    "revision": "42fa70b8bfc53216255b",
+    "url": "/static/js/runtime~main.5379b062.js"
+  }
+]);
